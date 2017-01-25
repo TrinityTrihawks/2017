07612286feab811895550fc4b2cffa50d9d1d681
Waweros <lion@waposta.com>
2017-01-24 18:04:06 -0600

    Added better build system

---
+++ src/org/usfirst/frc/team4215/Robot.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team4215;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+
+	public Robot() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
