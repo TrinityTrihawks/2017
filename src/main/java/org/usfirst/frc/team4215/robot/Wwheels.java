3bfb5879d585c32b6358d7c5ab32420eeb38d061
Waweros <lion@waposta.com>
2017-02-09 18:06:25 -0600

    Added mode-switching

--- src/main/java/org/usfirst/frc/team4215/robot/Wwheels.java
+++ src/main/java/org/usfirst/frc/team4215/robot/Wwheels.java
@@ -6,62 +6,76 @@ import edu.wpi.first.wpilibj.buttons.Button;
 	
 	public class Wwheels {
 		
-			private static Wwheels instance;
-			public static Wwheels Create() {
-				if (instance == null) {
-					instance = new Wwheels();
-				}
-				return instance;
-			}
-			private Wwheels() {
-				//21-24 declare talons
-				flWheel = new CANTalon(3);
-				frWheel = new CANTalon(0);
-				blWheel = new CANTalon(1);
-				brWheel = new CANTalon(2);			
-			}
-			
+		double wheelRadius = 6; // inches
+		double wheelCirc = 2*Math.PI*wheelRadius;
+		double secondsToMinutes = (double) 1/60; // seconds/minutes
+		
+		CANTalon.TalonControlMode controlMode;
+		
 		//21-24 declare talons
 		CANTalon flWheel;
 		CANTalon frWheel;
 		CANTalon blWheel;
 		CANTalon brWheel;
 		
+		private static Wwheels instance;
 		
+		public static Wwheels Create() {
+			if (instance == null) {
+				instance = new Wwheels();
+			}
+			return instance;
+		}
+		
+		private Wwheels() {
+			//21-24 declare talons
+			flWheel = new CANTalon(3);
+			frWheel = new CANTalon(0);
+			blWheel = new CANTalon(1);
+			brWheel = new CANTalon(2);			
+			}
 		
 		/**
-		 * @Author Carl and Will
-		 * @Purpose floor/ceiling for power and setting wheels
+		 *	Changes control modes of component  talons
+		 * @author Waweru and Carl 
+		 */
+		public void setTalonControlMode(CANTalon.TalonControlMode newMode){
+			controlMode = newMode;
+		}
+		
+		/**
+		 * 
+		 * @return
+		 */
+		public CANTalon.TalonControlMode getTalonCOntrolMode(){
+			return flWheel.getControlMode();
+			
+		}
+		
+		/**
+		 * floor/ceiling for power and setting wheels
+		 * @author Carl and Will 
 		 */
 		public void Go(double lFront, double lBack,double rFront, double rBack){
-			if (lFront > 1) {
-				lFront = 1;
-			} else if (lFront < -1) {
-				lFront = -1;
-			} 
-			
-			if (lBack > 1) {
-				lBack = 1;
-			} else if (lBack < -1) {
-				lBack = -1;
-			} 
-			
-			if (rFront > 1) {
-				rFront = 1;
-			} else if (rFront < -1) {
-				rFront = -1;
+			if(controlMode == CANTalon.TalonControlMode.Position){
+				lFront = lFront/wheelCirc;
+				lBack = lBack/wheelCirc;
+				rFront = rFront/wheelCirc;
+				rBack = rBack/wheelCirc;
+			}
+			if(controlMode == CANTalon.TalonControlMode.Speed){
+				lFront = lFront*secondsToMinutes/wheelCirc;
+				lBack = lBack*secondsToMinutes/wheelCirc;
+				rFront = rFront*secondsToMinutes/wheelCirc;
+				rBack = rBack*secondsToMinutes/wheelCirc;
 			}
 			
-			if (rBack > 1) {
-				rBack = 1;
-			} else if (rBack < -1) {
-				rBack = -1;
-			}
 			
 			frWheel.set(rFront);
 			brWheel.set(rBack);
 			flWheel.set(lFront);
 			blWheel.set(lBack);
+			
 		}
 		
 		public void Reset() {
