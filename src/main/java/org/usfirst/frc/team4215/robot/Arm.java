0672947d19cf99a8317070837a10e2bbc678a24a
Charles Nykamp <cqnykamp@gmail.com>
2017-02-13 16:18:43 -0600

    Methods that get arm power and position

--- src/main/java/org/usfirst/frc/team4215/robot/Arm.java
+++ src/main/java/org/usfirst/frc/team4215/robot/Arm.java
@@ -1,16 +1,40 @@
 package main.java.org.usfirst.frc.team4215.robot;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Victor;
 
 public class Arm {
 	
 	Victor arm = new Victor(1);
+	Encoder enc = new Encoder(1, 2, false);
+	//change the ports to whichever are the correct ones
+	//reversed may need to be 'true', depending on how encoder is set up
+	
+	public Arm() {
+		
+	}
+	
+	
 	
 	public void setArm(double power) {
 		if (power > 1) power = 1;
 		else if (power < -1) power = -1;
 		
 		arm.set(power);
+	}
+	
+	
+	public double getArmPosition() {
+		return enc.get() / 90;
+		//max rotation for arm will be 90 degrees
+		//total CPR is 360
+		//therefore, this will get a number between 0 and 1
+		
+	}
+	
+	
+	public double getArmPower() {
+		return arm.get();
 	}
 
 }
