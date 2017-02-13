236733e1231780e2e5f2ed08b95d5a1464b0aa3b
Stephen Norlien <stephen.norlien@gmail.com>
2017-02-13 16:25:31 -0600

    Modified robot.java to work with the winch

--- src/main/java/org/usfirst/frc/team4215/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4215/robot/Robot.java
@@ -1,11 +1,24 @@
 package main.java.org.usfirst.frc.team4215.robot;
 
+import org.usfirst.frc.team4215.robot.prototypes.Winch_Test;
+
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends IterativeRobot {
+	Joystick winchJoystick = new Joystick(0);
+Winch_Test winch = new Winch_Test();
+public void robotInit(){
+	}
 
-	public Robot() {
-		// TODO Auto-generated constructor stub
+	public void teleopInit() {
+	
+		double l = winchJoystick.getRawAxis(2);
+		
+
+		winch.set(l, l);
+		
 	}
 
 }
