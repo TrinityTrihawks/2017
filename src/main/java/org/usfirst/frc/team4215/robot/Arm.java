6fb0d5531e6b9a5f60517f1ce27f92cde8ac255b
Charles Nykamp <cqnykamp@gmail.com>
2017-02-14 16:39:10 -0600

    Changed some things

--- src/main/java/org/usfirst/frc/team4215/robot/Arm.java
+++ src/main/java/org/usfirst/frc/team4215/robot/Arm.java
@@ -1,17 +1,25 @@
 package main.java.org.usfirst.frc.team4215.robot;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Victor;
 
 public class Arm {
 	
-	Victor arm = new Victor(1);
-	Encoder enc = new Encoder(1, 2, false);
-	//change the ports to whichever are the correct ones
-	//reversed may need to be 'true', depending on how encoder is set up
+	private Victor arm;
+	private Encoder enc;
+	private DoubleSolenoid ds;
 	
 	public Arm() {
+		arm = new Victor(1);
 		
+		enc = new Encoder(1, 2, false);
+		//change the ports to whichever are the correct ones
+		//reversed may need to be 'true', depending on how encoder is set up
+		
+		ds = new DoubleSolenoid(1, 2);
+		//These are sample port numbers. Change them to the correct ones.
+		//might need to include moduleNumber in parameters
 	}
 	
 	
@@ -25,16 +33,38 @@ public class Arm {
 	
 	
 	public double getArmPosition() {
-		return enc.get() / 90;
-		//max rotation for arm will be 90 degrees
-		//total CPR is 360
-		//therefore, this will get a number between 0 and 1
+		return enc.get();
 		
+		
+		
+		//return enc.get() / 90;
+		
+			//max rotation for arm will be 90 degrees
+			//total CPR is 360
+			//therefore, this will get a number between 0 and 1
+			
 	}
 	
 	
 	public double getArmPower() {
 		return arm.get();
 	}
+	
+	
+	
+	public void armCompress() {
+		ds.set(DoubleSolenoid.Value.kForward);
+	}
+	
+	
+	public void armDecompress() {
+		ds.set(DoubleSolenoid.Value.kReverse);
+	}
+	
+	
+	public void armOff() {
+		ds.set(DoubleSolenoid.Value.kOff);
+	}
+	
 
 }
