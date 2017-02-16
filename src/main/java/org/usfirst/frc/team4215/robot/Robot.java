7b63a508431b77a13be40b6c506068c5288bb5c7
Ransom14 <ransom.r.schmidt@gmail.com>
2017-02-16 16:29:52 -0600

    solved commit problem

--- src/main/java/org/usfirst/frc/team4215/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4215/robot/Robot.java
@@ -2,12 +2,22 @@ package main.java.org.usfirst.frc.team4215.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import main.java.org.usfirst.frc.team4215.robot.steamworks.VisionTest;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.modifiers.TankModifier;
+import main.java.org.usfirst.frc.team4215.robot.Drivetrain;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.Joystick.AxisType;
+import com.ctre.CANTalon.FeedbackDevice;
+import com.ctre.CANTalon.TalonControlMode;
+import com.ctre.CANTalon;
 
-<<<<<<< Updated upstream
 public class Robot<VisonTest> extends IterativeRobot {
 
 	private VisionTest visionTest1;
-=======
 	Joystick leftStick = new Joystick(0);
 	Joystick rightStick = new Joystick(1);
 	Drivetrain drivetrain;
@@ -21,42 +31,51 @@ public class Robot<VisonTest> extends IterativeRobot {
 	@Override
 	public void robotInit() {
 	    drivetrain = Drivetrain.Create();
-<<<<<<< Updated upstream
-	    
-=======
->>>>>>> Stashed changes
-	
+
+		visionTest1 = new VisionTest();
+			visionTest1.visionInit();
+			System.out.println("Hello World");
 	}
 
 	@Override
 	public void autonomousInit() {
 		drivetrain.setTalonControlMode(TalonControlMode.Position);
-<<<<<<< Updated upstream
 		drivetrain.resetEncoder();
 		drivetrain.setPID(.05, 0, 0);
 		drivetrain.enableControl();
 		drivetrain.Go(24, 24, 24, 24);
-=======
-<<<<<<< Updated upstream
 		drivetrain.setPID(.1, 0, 0);
-=======
-
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+	}
+	double[] dist = new double[4];
+	/**
+	 * This function is called periodically during autonomous
+	 **/
+	@Override
+	public void autonomousPeriodic() {
+		dist = drivetrain.getDistance();
+		System.out.printf("Dist: %f \n",dist[0]);
 	}
 	
->>>>>>> Stashed changes
-	
-	public void robotInit(){
-	
-		visionTest1 = new VisionTest();
-			visionTest1.visionInit();
-			System.out.println("Hello World");
-	}
-	
-	public void teleopInit(){		
+	@Override
+	public void teleopInit(){
+		drivetrain.setTalonControlMode(TalonControlMode.PercentVbus);
+		drivetrain.disableControl();
 		visionTest1.visionStart();
 	}
+	
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
+		double left = leftStick.getRawAxis(1);
+		double right = leftStick.getRawAxis(3);
+		double strafe = leftStick.getRawAxis(2);
+		boolean isStrafing = leftStick.getRawButton(1);
+		
+		drivetrain.drive(left, right, strafe,isStrafing);
+	}
+
 	
 	public void disableInit() throws InterruptedException{
 			
