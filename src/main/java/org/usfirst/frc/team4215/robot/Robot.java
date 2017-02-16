456ca5165520eada70af5b5be81ef7aab8705c21
Ransom14 <ransom.r.schmidt@gmail.com>
2017-02-16 15:41:20 -0600

    Robot.java changes

--- src/main/java/org/usfirst/frc/team4215/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4215/robot/Robot.java
@@ -3,9 +3,49 @@ package main.java.org.usfirst.frc.team4215.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import main.java.org.usfirst.frc.team4215.robot.steamworks.VisionTest;
 
+<<<<<<< Updated upstream
 public class Robot<VisonTest> extends IterativeRobot {
 
 	private VisionTest visionTest1;
+=======
+	Joystick leftStick = new Joystick(0);
+	Joystick rightStick = new Joystick(1);
+	Drivetrain drivetrain;
+	
+	
+	
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+	    drivetrain = Drivetrain.Create();
+<<<<<<< Updated upstream
+	    
+=======
+>>>>>>> Stashed changes
+	
+	}
+
+	@Override
+	public void autonomousInit() {
+		drivetrain.setTalonControlMode(TalonControlMode.Position);
+<<<<<<< Updated upstream
+		drivetrain.resetEncoder();
+		drivetrain.setPID(.05, 0, 0);
+		drivetrain.enableControl();
+		drivetrain.Go(24, 24, 24, 24);
+=======
+<<<<<<< Updated upstream
+		drivetrain.setPID(.1, 0, 0);
+=======
+
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+	}
+	
+>>>>>>> Stashed changes
 	
 	public void robotInit(){
 	
