2601edf23d172cdd7af38a53aa82fb32b054a89b
Waweros <lion@waposta.com>
2017-02-17 16:32:46 -0600

    Fixed some wierd  errors

--- src/main/java/org/usfirst/frc/team4215/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4215/robot/Robot.java
@@ -1,15 +1,15 @@
 package main.java.org.usfirst.frc.team4215.robot;
 
-import org.usfirst.frc.team4215.robot.prototypes.Winch_Test;
-
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 
+
 public class Robot extends IterativeRobot {
 	Joystick winchJoystick = new Joystick(0);
-Winch_Test winch = new Winch_Test();
-public void robotInit(){
+	WinchTest winch;
+	public void robotInit(){
+		winch = new WinchTest();
 	}
 
 	public void teleopInit() {
--- src/main/java/org/usfirst/frc/team4215/robot/WinchTest.java
+++ src/main/java/org/usfirst/frc/team4215/robot/WinchTest.java
@@ -1,15 +1,16 @@
-package org.usfirst.frc.team4215.robot.prototypes;
+package src.main.java.org.usfirst.frc.team4215.robot;
 
 //import edu.wpi.first.wpilibj.GenericHID;
 //import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Victor;
 
-public class Winch_Test {
+public class WinchTest {
 	Victor winch1 = new Victor(0);						// sets where the first winch motor is controlled from
 	Victor winch2 = new Victor(1);						// sets where the second winch motor is controlled from
-//	Joystick armJoystick = new Joystick(0);				// sets which joystick controlls the winch motors
-	public Winch_Test () {
-	};
+	public WinchTest(){
+		
+		
+	}
 	public void set(double l, double r) {
 		
 //		GenericHID.Hand armJoystick = GenericHID.Hand.kRight;
