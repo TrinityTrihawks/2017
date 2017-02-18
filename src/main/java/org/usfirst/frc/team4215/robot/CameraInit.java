75a017a676b9f2ac6e03725cf6dfce3321cb68cc
Waweros <lion@waposta.com>
2017-02-18 15:20:33 -0600

    Moved camera initialization code

---
+++ src/main/java/org/usfirst/frc/team4215/robot/CameraInit.java
@@ -0,0 +1,60 @@
+package main.java.org.usfirst.frc.team4215.robot;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.AxisCamera;
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.first.wpilibj.CameraServer;
+
+public class CameraInit implements Runnable {
+
+	public CameraInit() {
+		
+	}
+
+	@Override
+	public void run() {
+		// Get the Axis camera from CameraServer
+		AxisCamera camera = CameraServer.getInstance().addAxisCamera("Front", "10.42.15.39");
+		// Set the resolution
+		camera.setResolution(640, 480);
+					
+		
+		AxisCamera cameraBack = CameraServer.getInstance().addAxisCamera("Back", "10.42.15.37");
+		// Set the resolution
+		cameraBack.setResolution(640, 480);
+				
+		// Get a CvSink. This will capture Mats from the camera
+		CvSink cvSink = CameraServer.getInstance().getVideo();
+		// Setup a CvSource. This will send images back to the Dashboard
+		CvSource outputStream = CameraServer.getInstance().putVideo("Rectangle", 640, 480);
+
+		// Mats are very memory expensive. Lets reuse this Mat.
+		Mat mat = new Mat();
+
+		// This cannot be 'true'. The program will never exit if it is. This
+		// lets the robot stop this thread when restarting robot code or
+		// deploying.
+		while (!Thread.interrupted()) {
+			// Tell the CvSink to grab a frame from the camera and put it
+			// in the source mat.  If there is an error notify the output.
+			if (cvSink.grabFrame(mat) == 0) {
+				// Send the output the error.
+				outputStream.notifyError(cvSink.getError());
+				// skip the rest of the current iteration
+				continue;
+			}
+			// Put a rectangle on the image
+			Imgproc.rectangle(mat, new Point(100, 100), new Point(400, 400),
+					new Scalar(255, 255, 255), 5);
+			// Give the output stream a new image to display
+			outputStream.putFrame(mat);
+		}
+
+	}
+
+}
