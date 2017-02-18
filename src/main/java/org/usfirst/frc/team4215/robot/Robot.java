746b147ee956b906f4d0ec62cf02a9868d03c180
Waweros <lion@waposta.com>
2017-02-17 18:01:27 -0600

    Fxed winch

--- src/main/java/org/usfirst/frc/team4215/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4215/robot/Robot.java
@@ -3,16 +3,16 @@ package main.java.org.usfirst.frc.team4215.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-
+import src.main.java.org.usfirst.frc.team4215.robot.WinchTest;
 
 public class Robot extends IterativeRobot {
 	Joystick winchJoystick = new Joystick(0);
 	WinchTest winch;
 	public void robotInit(){
-		winch = new WinchTest();
+	winch = new WinchTest();
 	}
 
-	public void teleopInit() {
+	public void teleopPeriodic() {
 	
 		double l = winchJoystick.getRawAxis(2);
 		
