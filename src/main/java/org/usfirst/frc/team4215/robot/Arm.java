1a07edb43298d724a799ebd02587b1803934d523
Waweros <lion@waposta.com>
2017-02-18 09:48:05 -0600

    Fixed Stash

---
+++ src/main/java/org/usfirst/frc/team4215/robot/Arm.java
@@ -0,0 +1,67 @@
+package main.java.org.usfirst.frc.team4215.robot;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Victor;
+
+public class Arm {
+	
+	private Victor armV;
+	private Encoder enc;
+	private DoubleSolenoid ds;
+	
+	public Arm() {
+		armV = new Victor(2);
+		
+		enc = new Encoder(1, 2, false);
+		//change the ports to whichever are the correct ones
+		//reversed may need to be 'true', depending on how encoder is set up
+		
+		ds = new DoubleSolenoid(6, 7);
+		//These are sample port numbers. Change them to the correct ones.
+		//might need to include moduleNumber in parameters
+	}
+	
+	
+	
+	public void setArm(double power) {
+		armV.set(power);
+	}
+	
+	
+	public double getArmPosition() {
+		return enc.get();
+		
+		
+		
+		//return enc.get() / 90;
+		
+			//max rotation for arm will be 90 degrees
+			//total CPR is 360
+			//therefore, this will get a number between 0 and 1
+			
+	}
+	
+	
+	public double getArmPower() {
+		return armV.get();
+	}
+	
+	
+	
+	public void armCompress() {
+		ds.set(DoubleSolenoid.Value.kForward);
+	}
+	
+	
+	public void armDecompress() {
+		ds.set(DoubleSolenoid.Value.kReverse);
+	}
+	
+	
+	public void armOff() {
+		ds.set(DoubleSolenoid.Value.kOff);
+	}
+	
+
+}
