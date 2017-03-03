e1390a277056843c0e682d260cf834f10a815bca
jmcshane <jmcshan1@gmail.com>
2017-03-03 13:15:44 -0600

    Adding spock tests as an example from simple read

---
+++ src/test/groovy/prototypes/SimpleReadSpockTest.groovy
@@ -0,0 +1,43 @@
+package prototypes
+
+
+import java.nio.charset.StandardCharsets
+
+import org.junit.Assert
+import org.springframework.test.util.ReflectionTestUtils
+
+import gnu.io.SerialPortEvent
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class SimpleReadSpockTest extends Specification {
+
+	SimpleRead simpleRead
+	SerialPortEvent mockEvent
+	
+	def 'setup'() {
+		simpleRead = new SimpleRead()
+		mockEvent = Mock()
+	}
+
+	@Unroll("#input should return #output")
+	def "input returns output"() {
+		given:
+		InputStream inputStream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8))
+		ReflectionTestUtils.setField(simpleRead, "inputStream", inputStream)
+		
+		when:
+		simpleRead.serialEvent(mockEvent);
+		
+		then:
+		1* mockEvent.getEventType() >> SerialPortEvent.DATA_AVAILABLE
+		int distance = simpleRead.getDistance();
+		Assert.assertEquals(output, distance);
+
+		where:
+		input     | output
+		'\rR1424' | 1424
+		'R1424\r' | 1424
+		'R0023\r' | 23
+	}
+}
