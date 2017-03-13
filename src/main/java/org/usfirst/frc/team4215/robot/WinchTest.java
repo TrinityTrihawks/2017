39d1f9aba24cffe56dc0d906bc8c9b7ed7a8f009
Waweros <lion@waposta.com>
2017-03-13 17:02:34 -0500

    Some readability issuees

--- src/main/java/org/usfirst/frc/team4215/robot/WinchTest.java
+++ src/main/java/org/usfirst/frc/team4215/robot/WinchTest.java
@@ -7,10 +7,7 @@ import edu.wpi.first.wpilibj.Victor;
 public class WinchTest {
 	Victor winch1 = new Victor(0);						// sets where the first winch motor is controlled from
 	Victor winch2 = new Victor(4);						// sets where the second winch motor is controlled from
-	public WinchTest(){
-		
-		
-	}
+
 	public void set(double l) {
 		if(l <0){
 			l = 0;
