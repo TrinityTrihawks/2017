120b9ddb8c1729832dd851cd18957f49794c8d7f
Ransom14 <ransom.r.schmidt@gmail.com>
2017-04-06 14:46:34 -0500

    RobotPropeties cass

---
+++ src/main/java/org/usfirst/frc/team4215/robot/RobotProperties.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team4215.robot;
+
+public class RobotProperties {
+
+	public static int Drive_Right_Joystick_id = 1;
+	public static int Drive_Left_Joystick_id = 3;
+	public static int Strafe_id = 8;
+	
+
+}
