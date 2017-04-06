02402b0a5f1ab46043ee164fdaa07e79a2fd39b4
Waweros <lion@waposta.com>
2017-04-06 09:12:54 -0500

    Uncommented gyroTurn

--- src/main/java/org/usfirst/frc/team4215/robot/AutonomousCommandLeft.java
+++ src/main/java/org/usfirst/frc/team4215/robot/AutonomousCommandLeft.java
@@ -9,7 +9,7 @@ public class AutonomousCommandLeft extends CommandGroup {
 	AutonomousCommandLeft(AxisCamera cameraFront){
 		//addSequential(new CommandDrive(41.5,450));
 		addSequential(new CommandDrive(180, 10));
-		//addSequential(new CommandTurn(60));
+		addSequential(new CommandTurn(60));
 		//addSequential(new CommandDrive(24, 270));
 	}
 
