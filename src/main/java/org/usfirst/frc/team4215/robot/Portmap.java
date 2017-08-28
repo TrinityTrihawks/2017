705a51f5323c7f5a3613feb88723f67af4eccbfe
Unknown <ransom.r.schmidt@gmail.com>
2017-08-28 10:00:07 -0500

    revverted something

--- src/main/java/org/usfirst/frc/team4215/robot/Portmap.java
+++ src/main/java/org/usfirst/frc/team4215/robot/Portmap.java
@@ -10,19 +10,10 @@ public class Portmap {
 	public static final int STRAFE_DRIVE_ID = 0;
 	public static final int DRIVE_LEFT_TOP_TRIGGER_ID = 5;
 	public static final int DRIVE_LEFT_BOTTOM_TRIGGER_ID = 7;
-	public static final int ARM_COMPRESS_BUTTON_ID = 1;
-	public static final int ARM_DECOMPRESS_BUTTON_ID = 2;
+	public static final int ARM_COMPRESS_BUTTON_ID = 0;
+	public static final int ARM_DECOMPRESS_BUTTON_ID = 1;
 	public static final int VICTOR_WINCH_ID1 = 0;
 	public static final int VICTOR_WINCH_ID2 = 4;
 	public static final int VICTOR_ARM_ID = 1;
 	public static final int LIMITSWITCH_DIO_ID = 0;
-	public static final int Talon_Front_Left_ID = 4;
-	public static final int Talon_Front_Right_ID = 1;
-	public static final int Talon_Back_Left_ID = 3;
-	public static final int	Talon_Back_Right_ID = 2;
-	public static final int PDP_Bus_Channel_Front_Left = 12;
-	public static final int PDP_Bus_Channel_Front_Right = 3;
-	public static final int PDP_Bus_Channel_Back_Left = 15;
-	public static final int PDP_Bus_Channel_Back_Right = 0;
-
 }
